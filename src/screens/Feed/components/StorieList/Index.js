262437818b416461d789f5d81a70ca65846baf32
Hermano Fernadnes <hermano@avanz.com.br>
2022-04-06 15:14:35 -0300

    fix storieList

--- src/screens/Feed/components/StorieList/Index.js
+++ src/screens/Feed/components/StorieList/Index.js
@@ -3,51 +3,57 @@ import React from 'react'
 
 import { LinearGradient } from 'expo-linear-gradient';
 
+import { StyledText, ProfilePic, TextContainer, StorieContainer} from './style'
 
 const Index = ({storie, key, navigation}) => {
   return (
     <View key={key} >
         {storie.closeFriend ? (
-            <TouchableOpacity onPress={() => {navigation.navigate('Stories')}} style={storieStyles.storieContainer}>
+            <StorieContainer onPress={() => {navigation.navigate('Stories')}}>
 
-                <View style={[
-                    storie.visualized
-                    ? storieStyles.visualized
-                    : storieStyles.closeFriend
-                ]} >
-                  <Image 
-                      style={storieStyles.profilePic} 
-                      source={{uri: `https://insta-clone-hermano.herokuapp.com/profile/${storie.user.profile_path}`}}
-                  />
-               
-                </View>
-
-                <View style={storieStyles.textContainer} >
-                  <Text style={storieStyles.text} >Storie {key}</Text>
-                </View>
-            </TouchableOpacity>
+                {storie.visualized ? (
+                  <Visualized>
+                    <ProfilePic 
+                        source={{uri: `https://insta-clone-hermano.herokuapp.com/profile/${storie.user.profile_path}`}}
+                    />
+                  </Visualized>
+                ):(
+                  <CloseFriend>
+                    <ProfilePic 
+                        source={{uri: `https://insta-clone-hermano.herokuapp.com/profile/${storie.user.profile_path}`}}
+                    />
+                  </CloseFriend>
+                )}
+                <TextContainer>
+                  <StyledText>Storie {key}</StyledText>
+                </TextContainer>
+            </StorieContainer>
         ) : (
-            <TouchableOpacity onPress={() => {navigation.navigate('Stories')}} style={storieStyles.storieContainer}>
+            <StorieContainer onPress={() => {navigation.navigate('Stories')}}>
                 
-                <LinearGradient 
-                colors={['#f09433', '#e6683c', '#dc2743', '#cc2366', '#bc1888']}
-                style={[
-                    storieStyles.noVisualizedContainer,
-                    storie.visualized && storieStyles.visualized
-                ]}
-                
-                >
-                <Image 
-                    style={storieStyles.profilePic} 
-                    source={{uri: `https://insta-clone-hermano.herokuapp.com/profile/${storie.user.profile_path}`}}
-                />
-              
-                </LinearGradient>
-
-                <View style={storieStyles.textContainer} >
-                  <Text style={storieStyles.text} >Storie {key}</Text>
-                </View>
-            </TouchableOpacity>
+                {storie.visualized ? (
+                  <LinearGradient 
+                    colors={['#f09433', '#e6683c', '#dc2743', '#cc2366', '#bc1888']}
+                    style={storieStyles.visualized}
+                  >
+                    <ProfilePic 
+                        source={{uri: `https://insta-clone-hermano.herokuapp.com/profile/${storie.user.profile_path}`}}
+                    />
+                  </LinearGradient>
+                ):(
+                  <LinearGradient 
+                    colors={['#f09433', '#e6683c', '#dc2743', '#cc2366', '#bc1888']}
+                    style={storieStyles.noVisualizedContainer}
+                  >
+                    <ProfilePic 
+                        source={{uri: `https://insta-clone-hermano.herokuapp.com/profile/${storie.user.profile_path}`}}
+                    />
+                  </LinearGradient>
+                )}
+                <TextContainer>
+                  <StyledText>Storie {key}</StyledText>
+                </TextContainer>
+            </StorieContainer>
         )}
         
     </View>
@@ -57,21 +63,6 @@ const Index = ({storie, key, navigation}) => {
 export default Index
 
 const storieStyles = StyleSheet.create({
-
-    storieContainer:{
-      marginHorizontal: 5,
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    closeFriend:{
-      alignItems: 'center',
-      justifyContent: 'center',
-      borderRadius: 100,
-      height: 54,
-      width: 54,
-      backgroundColor:'#1d9b21',
-      padding: 2,
-    },
     visualized:{
       alignItems: 'center',
       justifyContent: 'center',
@@ -81,6 +72,7 @@ const storieStyles = StyleSheet.create({
       backgroundColor:'#ddd',
       padding: 1,
     },
+
     noVisualizedContainer:{
       alignItems: 'center',
       justifyContent: 'center',
@@ -88,23 +80,6 @@ const storieStyles = StyleSheet.create({
       borderRadius: 100,
       height: 55,
       width: 55,
-    
     },  
-  
-    profilePic:{
-      height: '100%',
-      width: '100%',
-      borderWidth: 4,
-      borderColor: '#000',
-      borderRadius: 100,
-    },
-    textContainer:{
-  
-    },
-    text: {
-      fontSize: 13,
-      marginTop: 5,
-      color: '#FFFFFF',
-      textAlign: 'center',
-    }
-  })
+
+})
--- src/screens/Feed/components/StorieList/style.js
+++ src/screens/Feed/components/StorieList/style.js
@@ -1,61 +1,57 @@
 import styled from 'styled-components/native';
-import Theme from '../../Theme';
-import {Dimensions} from "react-native";
-
-let width = Dimensions.get('window').width;
 
 
-
-  storieContainer = styled.TouchableOpacity`
-    marginHorizontal: '5px',
-    alignItems: 'center',
-    justifyContent: 'center',
+export const StorieContainer = styled.TouchableOpacity`
+    marginHorizontal: 5px;
+    align-items: center;
+    justifyContent: center;
   `
   
-  closeFriend = styled.View`
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 100,
-    height: '54px',
-    width: '54px',
-    backgroundColor:'#1d9b21',
-    padding: '2px',
+  export const CloseFriend = styled.View`
+    align-items: center;
+    justifyContent: center;
+    borderRadius: 100;
+    height: 54px;
+    width: 54px;
+    backgroundColor:#1d9b21;
+    padding: 2px;
   `
 
-  visualized = styled.View`
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 100,
-    height: '54px',
-    width: '54px',
-    backgroundColor:'#ddd',
-    padding: '1px',
+  export const Visualized = styled.View`
+    align-items: center;
+    justifyContent: center;
+    borderRadius: 100;
+    height: 54px;
+    width: 54px;
+    backgroundColor:#ddd;
+    padding: 1px;
   `
 
-  noVisualizedContainer = styled.View`
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: '2px',
-    borderRadius: 100,
-    height: '55px',
-    width: '55px',
+  export const NoVisualizedContainer = styled.View`
+    align-items: center;
+    justifyContent: center;
+    padding: 2px;
+    borderRadius: 100;
+    height: 55px;
+    width: 55px;
   
   `  
 
-  profilePic = styled.Image`
-    height: '100%',
-    width: '100%',
-    borderWidth: 4,
-    borderColor: '#000',
-    borderRadius: 100,
+  export const ProfilePic = styled.Image`
+    height: 100%;
+    width: 100%;
+    borderWidth: 4;
+    borderColor: #000;
+    borderRadius: 100;
   `
-  textContainer = styled.View`
+
+  export const TextContainer = styled.View`
 
   `
 
-  text = styled.Text`
-    fontSize: 13,
-    marginTop: '5px',
-    color: '#FFFFFF',
-    textAlign: 'center',
+  export const StyledText = styled.Text`
+    fontSize: 13;
+    marginTop: 5px;
+    color: #FFFFFF;
+    textAlign: center;
   `
